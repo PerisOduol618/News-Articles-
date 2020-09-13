98550c22f4a772ef7c0f40f9d0b6bb34f4213802
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-13 15:10:23 +0300

    add Configurations

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://api.thenewsdb.org/3/news/{}?api_key={}'
     pass 
 
 class ProdConfig(Config):
