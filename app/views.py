55f503108d994f3a732a501929759f905f4c0b94
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-13 15:12:00 +0300

    Add View news page function that returns the news details page and its data

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
 from flask import render_template
-from app import app
 
 
-@app.route('/')
+@app.route('/news/<int:news_id>')
+def index(news_id):
+    '''
+    View news page function that returns the news details page and its data
+    '''
+
+    return render_template('news.html',id = news_id)
+    
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Hello welcome to News Article'
+    return render_template('index.html', title = title)
