d18c61db82412e3377be5c542b7db1b821aa5b63
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-14 23:34:13 +0300

    Add __init_.py and views.py

---
+++ app/main/error.py
@@ -0,0 +1,9 @@
+# from flask import render_template
+
+
+# @app.errorhandler(404)
+# def four_Ow_four(error):
+#     '''
+#     Function to render the 404
+#     '''
+#     return render_template('fourOwfour.html'),404
