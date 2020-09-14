1a5a0d1fad5c2923a5219a627938572f01474e15
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-14 23:35:21 +0300

    Add manage.py and start.sh

--- app/request.py
+++ app/request.py
@@ -1,19 +1,26 @@
-from app import app
 import urllib.request
 import json
-from .models import news
+from .models import Sources, Articles
 
-Sources = news.Sources
-Articles = news.Articles
+# Sources = news.Sources
+# Articles = news.Articles
 
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 # Getting the news base url
-base_url = app.config['NEWS_SOURCES_BASE_URL']
-articles_base_url = app.config['ARTICLES_BASE_URL']
+base_url =None
 
+# Getting the news articles base url
+articles_base_url = None
+
+
+def configure_request(app):
+    global api_key,base_url,articles_base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_SOURCES_BASE_URL']
+    articles_base_url = app.config['ARTICLES_BASE_URL']
 
 def get_sources(category):
     '''
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export NEWS_API_KEY=37ef61ec686546e0969631a9045a9f70
+ export SECRET_KEY=fn0AyPYzJhHBZBafcFbfyA
+
+ python3.6 manage.py server
